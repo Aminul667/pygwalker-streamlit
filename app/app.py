58e6bca5bbad030d8bdf1beb0222a7d148541769
Aminul667 <rahathssn@gmail.com>
2023-07-22 13:42:00 +0600

    added streamlit component

--- app/app.py
+++ app/app.py
@@ -3,6 +3,20 @@ import pandas as pd
 import streamlit.components.v1 as components
 import streamlit as st
 
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="Use Pygwalker In Streamlit",
+    layout="wide"
+)
+
+# Add Title
+st.title("Use Pygwalker In Streamlit")
+
+# Import your data
 df = pd.read_csv("app/data/airbnb.csv")
 
-print(df.head())
+# Generate the HTML using Pygwalker
+pyg_html = pyg.walk(df, return_html=True)
+
+# Embed the HTML into the Streamlit app
+components.html(pyg_html, height=1000, scrolling=True)
